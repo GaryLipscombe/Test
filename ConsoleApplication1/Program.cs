a1c4f83ac2f5550407486dc9f2b731042402c377
GaryLipscombe <gary.lipscombe@gmail.com>
2015-03-16 18:53:33 +0000

    1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Enter a number....");
+            number = int.Parse(Console.ReadLine());
+
+            while(number<50)
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
